8711877e37351bdd3f7e042c59776b96b474a312
Magesh Sundaravel <magesh094@gmail.com>
2026-01-14 17:44:48 +0100

    OS Module

--- src/modules/os/basics.py
+++ src/modules/os/basics.py
@@ -4,8 +4,13 @@ import os
 def get_cwd():
     print(os.getcwd())
 
+def change_dir() :
+    os.chdir("/Users/mageshlookalike/Documents/Personal/AI/Python/src/modules")
+
 
 def main():
+    get_cwd()
+    change_dir()
     get_cwd()
 
 
